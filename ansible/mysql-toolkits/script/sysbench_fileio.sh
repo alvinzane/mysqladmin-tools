813215c5d165ab62bd0bfb399f03704019ba0039
mysql-dev-fun <zengywei@gmail.com>
2018-07-07 01:45:53 +0800

    sysbench 模板

--- ansible/mysql-toolkits/script/sysbench_fileio.sh
+++ ansible/mysql-toolkits/script/sysbench_fileio.sh
@@ -1,1 +1,18 @@
 #!/usr/bin/env bash
+# test mode {seqwr, seqrd, seqrewr, rndrd, rndwr, rndrw}
+export LD_LIBRARY_PATH=/usr/local/mysql/lib/
+
+THREADS=$1
+MODE=rndwr
+TEST_NAME=fileio
+
+ARGS="
+--file-num=2
+--file-test-mode=$MODE
+--report-interval=10
+--threads=$THREADS
+--time=30
+"
+sysbench $TEST_NAME $ARGS prepare
+sysbench $TEST_NAME $ARGS run | tee -a  ~/log/fileio_"$MODE"_X_"$THREADS"_`date +"%Y%m%d_%H%M%S"`.log
+sysbench $TEST_NAME $ARGS cleanup
--- ansible/mysql-toolkits/script/sysbench_mysql_oltp.sh
+++ ansible/mysql-toolkits/script/sysbench_mysql_oltp.sh
@@ -3,35 +3,22 @@
 # sysbench /usr/local/sysbench/share/sysbench/oltp_read_write.lua help
 export LD_LIBRARY_PATH=/usr/local/mysql/lib/
 
-HOST=192.168.20.101
-PORT=3306
-USER=admin
-PASSWORD=aaaaaa
-
 TEST_NAME=/usr/local/sysbench/share/sysbench/oltp_read_write.lua
 THREADS=1
 
-sysbench $TEST_NAME \
---mysql-host=$HOST \
---mysql-port=$PORT \
---mysql-user=$USER \
---mysql-password=$PASSWORD \
---mysql-db=sbtest \
---table_size=50000 \
---tables=10 \
---report-interval=5 \
---threads=$THREADS \
---time=60 \
-prepare
+ARGS="
+--mysql-host=192.168.20.101
+--mysql-port=3306
+--mysql-user=admin
+--mysql-password=aaaaaa
+--mysql-db=sbtest
+--table_size=50000
+--tables=10
+--report-interval=5
+--threads=$THREADS
+--time=60
+"
 
-sysbench $TEST_NAME \
---mysql-host=$HOST \
---mysql-port=$PORT \
---mysql-user=$USER \
---mysql-password=$PASSWORD \
---table_size=50000 \
---tables=10 \
---report-interval=5 \
---threads=$THREADS \
---time=60 \
-run | tee -a  ~/log/sysbench_oltp_read_writeX_"$THREADS"_`date +"%Y%m%d_%H%M%S"`.log
+sysbench $TEST_NAME $ARGS prepare
+sysbench $TEST_NAME $ARGS run | tee -a  ~/log/sysbench_oltp_read_write_X_"$THREADS"_`date +"%Y%m%d_%H%M%S"`.log
+sysbench $TEST_NAME $ARGS cleanup
