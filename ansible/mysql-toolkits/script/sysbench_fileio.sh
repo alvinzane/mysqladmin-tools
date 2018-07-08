8fd43eaf048f659729601b349872e65f2186898a
mysql-dev-fun <zengywei@gmail.com>
2018-07-08 21:50:54 +0800

    sysbench 模板 & mysqldump playbook

--- ansible/mysql-toolkits/script/sysbench_fileio.sh
+++ ansible/mysql-toolkits/script/sysbench_fileio.sh
@@ -2,8 +2,11 @@
 # test mode {seqwr, seqrd, seqrewr, rndrd, rndwr, rndrw}
 export LD_LIBRARY_PATH=/usr/local/mysql/lib/
 
-THREADS=$1
-MODE=rndwr
+# 第一个参数为线程数,默认1
+if [ -z $1 ]; then THREADS=1; else THREADS=$1; fi
+# 第二个参数为测试时间,默认1分钟
+if [ -z $2 ]; then TIME=30; else TIME=$2; fi
+MODE=seqwr
 TEST_NAME=fileio
 
 ARGS="
@@ -11,7 +14,7 @@ ARGS="
 --file-test-mode=$MODE
 --report-interval=10
 --threads=$THREADS
---time=30
+--time=$TIME
 "
 sysbench $TEST_NAME $ARGS prepare
 sysbench $TEST_NAME $ARGS run | tee -a  ~/log/fileio_"$MODE"_X_"$THREADS"_`date +"%Y%m%d_%H%M%S"`.log
--- ansible/mysql-toolkits/script/sysbench_mysql_oltp.sh
+++ ansible/mysql-toolkits/script/sysbench_mysql_oltp.sh
@@ -1,24 +1,77 @@
 #!/usr/bin/env bash
 
-# sysbench /usr/local/sysbench/share/sysbench/oltp_read_write.lua help
-export LD_LIBRARY_PATH=/usr/local/mysql/lib/
+# 默认值
+HOST=127.0.0.1
+PORT=3306
+USER=admin
+PASSWORD=
+SOCKET=/tmp/mysql3306.sock
+DB=dbtest1
 
-TEST_NAME=/usr/local/sysbench/share/sysbench/oltp_read_write.lua
-THREADS=1
+# 短选项值
+while getopts "h:u:P:p:S:" arg
+do
+    case $arg in
+        h) HOST=$OPTARG ;;
+        P) PORT=$OPTARG ;;
+        p) PASSWORD=$OPTARG ;;
+        u) USER=$OPTARG ;;
+        S) SOCKET=$OPTARG ;;
+        ?)
+            echo "Usage: `basename $0` [-h host -P port -s socket -u user -p password] THREADS TIME"
+            exit 1
+            ;;
+    esac
+done
+
+# 去掉选项后,再解析参数
+shift $((OPTIND-1))
+# 第一个参数为线程数,默认1
+if [ -z $1 ]; then THREADS=1; else THREADS=$1; fi
+# 第二个参数为测试时间,默认1分钟
+if [ -z $2 ]; then TIME=60; else TIME=$2; fi
+
+echo "HOST:" $HOST
+echo "PORT:" $PORT
+echo "THREADS:" $THREADS
+echo "TIME:" $TIME
 
 ARGS="
---mysql-host=192.168.20.101
---mysql-port=3306
---mysql-user=admin
---mysql-password=aaaaaa
---mysql-db=sbtest
---table_size=50000
---tables=10
---report-interval=5
+--mysql-host=$HOST
+--mysql-user=$USER
+--mysql-port=$PORT
+--mysql-password=$PASSWORD
+--mysql-db=$DB
+--report-interval=10
+--time=$TIME
 --threads=$THREADS
---time=60
 "
 
-sysbench $TEST_NAME $ARGS prepare
-sysbench $TEST_NAME $ARGS run | tee -a  ~/log/sysbench_oltp_read_write_X_"$THREADS"_`date +"%Y%m%d_%H%M%S"`.log
-sysbench $TEST_NAME $ARGS cleanup
+# OLTP参数含义查看:
+# sysbench /usr/local/sysbench/share/sysbench/oltp_read_write.lua help
+OLTP_ARGS="
+--auto_inc=on
+--create_secondary=on
+--delete_inserts=1
+--distinct_ranges=1
+--index_updates=1
+--mysql_storage_engine=innodb
+--non_index_updates=0
+--order_ranges=1
+--point_selects=10
+--range_selects=on
+--range_size=100
+--secondary=off
+--simple_ranges=1
+--skip_trx=on
+--sum_ranges=1
+--table_size=10
+--tables=2
+"
+
+TESTNAME=/usr/local/sysbench/share/sysbench/oltp_read_only.lua
+#TESTNAME=/usr/local/sysbench/share/sysbench/oltp_read_write.lua
+
+sysbench $TESTNAME $ARGS $OLTP_ARGS prepare
+sysbench $TESTNAME $ARGS $OLTP_ARGS run | tee -a /tmp/"$HOST"_`basename $TESTNAME .lua`_"$THREADS"_`date +%Y%m%d_%H%M%S`.log
+sysbench $TESTNAME $ARGS $OLTP_ARGS cleanup
