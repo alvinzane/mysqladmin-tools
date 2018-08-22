78052dbae3f26a7b5f3d9e7394ea9e381e3892ac
mysql-dev-fun <zengywei@gmail.com>
2018-08-22 23:06:59 +0800

    mha 自动配置 ssh key

--- ansible/common/mysql/templates/setup.sql
+++ ansible/common/mysql/templates/setup.sql
@@ -3,8 +3,9 @@ alter user user() identified by '{{mysql_password}}';
 
 -- 创建 admin 用户
 create user 'admin'@'192.168.%.%' identified by '{{mysql_password}}';
-grant all on *.* to 'admin'@'192.168.%.%';
+grant all on *.* to 'admin'@'192.168.%.%' with GRANT OPTION;
 
 -- 创建 app 用户
 create user 'app'@'192.168.%.%' identified by '{{mysql_password}}';
-grant select,insert,update,delete,index,create,drop on *.* to 'app'@'192.168.%.%';
+grant select,insert,update,delete,index,create,drop,alter on *.* to 'app'@'192.168.%.%';
+grant TRIGGER,EXECUTE  on *.* to 'app'@'192.168.%.%';
