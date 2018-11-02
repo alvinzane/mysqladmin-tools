401f43290a93bcfe2f8384c574ae7f3e43e8047d
mysql-dev-fun <zengywei@gmail.com>
2018-11-02 22:42:39 +0800

    PXC

--- ansible/mysql-pxc/templates/create_replication_user.sql
+++ ansible/mysql-pxc/templates/create_replication_user.sql
@@ -4,6 +4,6 @@ create user {{ mysql_repl_user }}@'192.168.%.%' identified by '{{ mysql_repl_pas
 grant replication slave on *.* to {{ mysql_repl_user }}@'192.168.%.%';
 
 create user {{ wsrep_sst_auth_user }}@'localhost' identified by '{{ wsrep_sst_auth_pwd }}';
-grant reload,lock tables,replication client,process  on *.* to {{ wsrep_sst_auth_user }}@'192.168.%.%';
+grant reload,lock tables,replication client,process  on *.* to {{ wsrep_sst_auth_user }}@'localhost';
 
 SET SQL_LOG_BIN=1;
