fc0a81f8e2e16ffea32310b5b5ba9c25bc24996d
mysql-dev-fun <zengywei@gmail.com>
2018-11-07 21:45:46 +0800

    start_mysql.sh 优化,增加 更多参数,如 --wsrep-new-cluster

--- ansible/common/mysql/templates/start_mysql.sh
+++ ansible/common/mysql/templates/start_mysql.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
+
+# 第一个参数为port,其它参数放进 args
 port=$1
+shift
+args=$@
+
 if [ -z $port ]; then
     echo "Useage: start_mysql.sh port"
     exit 1
 fi
 running=`ps aux|grep -v grep |grep -E "mysqld.*$port" |wc -l`
-cmd="{{mysql_basedir}}/bin/mysqld --defaults-file={{ mysql_datahome }}/$port/my.cnf"
+cmd="{{mysql_basedir}}/bin/mysqld --defaults-file={{ mysql_datahome }}/$port/my.cnf $args"
 
 if [ "$running" = "0" ]; then
     echo "MySQL $port start."
