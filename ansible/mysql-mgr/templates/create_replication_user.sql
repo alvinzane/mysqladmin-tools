8c6a4c62ba517c16324695078f96c90d5b3a6797
mysql-dev-fun <zengywei@gmail.com>
2018-11-07 14:09:33 +0800

    mgr 增加 监控帐号

--- ansible/mysql-mgr/templates/create_replication_user.sql
+++ ansible/mysql-mgr/templates/create_replication_user.sql
@@ -1,5 +1,10 @@
 SET SQL_LOG_BIN=0;
+
 create user {{ mysql_repl_user }}@'192.168.%.%' identified by '{{ mysql_repl_password }}';
 grant replication slave on *.* to {{mysql_repl_user}}@'192.168.%.%';
+
+create user monitor@'192.168.%.%' identified by 'monitorpwd';
+grant replication client  on *.* to monitor@'192.168.%.%';
+
 SET SQL_LOG_BIN=1;
 
