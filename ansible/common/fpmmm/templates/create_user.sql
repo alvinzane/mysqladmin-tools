6255acba5ca80723aaece61056f9e9a2532bf6d7
mysql-dev-fun <zengywei@gmail.com>
2018-11-12 14:35:37 +0800

    fpmmm 完善

--- ansible/common/fpmmm/templates/create_user.sql
+++ ansible/common/fpmmm/templates/create_user.sql
@@ -1,4 +1,4 @@
-CREATE USER 'fpmmm'@'127.0.0.1' IDENTIFIED BY 'secret';
-GRANT PROCESS ON *.* TO 'fpmmm'@'127.0.0.1';
-GRANT REPLICATION CLIENT ON *.* TO 'fpmmm'@'127.0.0.1';
-GRANT SELECT ON `mysql`.`user` TO 'fpmmm'@'127.0.0.1';
+CREATE USER '{{fpmmm_user}}'@'127.0.0.1' IDENTIFIED BY '{{fpmmm_pwd}}';
+GRANT PROCESS ON *.* TO '{{fpmmm_user}}'@'127.0.0.1';
+GRANT REPLICATION CLIENT ON *.* TO '{{fpmmm_user}}'@'127.0.0.1';
+GRANT SELECT ON `mysql`.`user` TO '{{fpmmm_user}}'@'127.0.0.1';
