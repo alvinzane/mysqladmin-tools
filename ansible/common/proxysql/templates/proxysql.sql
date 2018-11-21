958756bb7b4dbba085a0c87463d4e1752700a8bf
mysql-dev-fun <zengywei@gmail.com>
2018-11-21 16:56:29 +0800

    proxysql配置脚本修改

--- ansible/common/proxysql/templates/proxysql.sql
+++ ansible/common/proxysql/templates/proxysql.sql
@@ -17,26 +17,26 @@ INSERT INTO mysql_servers (hostname,hostgroup_id,port,weight) VALUES ('{{ node }
 
 {% if rep_type == 'pxc' %}
 INSERT INTO mysql_galera_hostgroups(writer_hostgroup, backup_writer_hostgroup, reader_hostgroup, offline_hostgroup, active, max_transactions_behind)
- VALUES ({{mysql_port}}10,{{mysql_port}}20,{{mysql_port}}30,{{mysql_port}}90,1,300);
+ VALUES ({{mysql_port}}10,{{mysql_port}}11,{{mysql_port}}20,{{mysql_port}}90,1,300);
 {% endif %}
 
 {% if rep_type == 'mgr' %}
 INSERT INTO mysql_group_replication_hostgroups(writer_hostgroup, backup_writer_hostgroup, reader_hostgroup, offline_hostgroup, active, max_transactions_behind)
- VALUES ({{mysql_port}}10,{{mysql_port}}20,{{mysql_port}}30,{{mysql_port}}90,1,300);
+ VALUES ({{mysql_port}}10,{{mysql_port}}11,{{mysql_port}}20,{{mysql_port}}90,1,300);
 {% endif %}
 
 {% if rep_type == 'rep' %}
- INSERT INTO mysql_replication_hostgroups(writer_hostgroup, reader_hostgroup)
- VALUES ({{mysql_port}}10,{{mysql_port}}20);
+ INSERT INTO mysql_replication_hostgroups(writer_hostgroup, reader_hostgroup, check_type)
+ VALUES ({{mysql_port}}10,{{mysql_port}}20,'read_only');
 {% endif %}
 
 LOAD MYSQL SERVERS TO RUNTIME;
 SAVE MYSQL SERVERS TO DISK;
 
 -- 读写分离
-INSERT INTO mysql_query_rules (rule_id,active,match_digest,destination_hostgroup,apply)
+INSERT INTO mysql_query_rules (username,active,match_digest,destination_hostgroup,apply)
 VALUES
-(1,1,'^SELECT.*FOR UPDATE$',{{mysql_port}}10,1),
-(2,1,'^SELECT',{{mysql_port}}20,1);
+('{{mysql_app_user}}',1,'^SELECT.*FOR UPDATE$',{{mysql_port}}10,1),
+('{{mysql_app_user}}',1,'^SELECT',{{mysql_port}}20,1);
 LOAD MYSQL QUERY RULES TO RUNTIME;
 SAVE MYSQL QUERY RULES TO DISK;
